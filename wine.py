789b0a09632dd8523e797f5bc12c029fc3dd3d64
bululuu <64853085+bululuu@users.noreply.github.com>
2020-07-02 09:43:09 +0800

    Add files via upload

---
+++ wine.py
@@ -0,0 +1,135 @@
+from __future__ import division
+from __future__ import print_function
+
+import os
+import sys
+from time import time
+import numpy as np
+from numpy import percentile
+import matplotlib.pyplot as plt
+import matplotlib.font_manager
+
+# 导入
+from pyod.models.abod import ABOD
+from pyod.models.cblof import CBLOF
+from pyod.models.feature_bagging import FeatureBagging
+from pyod.models.hbos import HBOS
+from pyod.models.iforest import IForest
+from pyod.models.knn import KNN
+from pyod.models.lof import LOF
+from pyod.models.mcd import MCD
+from pyod.models.ocsvm import OCSVM
+from pyod.models.pca import PCA
+from pyod.models.lscp import LSCP
+
+# 读取文件
+for info in os.listdir(‘C:\Users\bululu\Documents\Untitled Folder\wine\benchmarks’): 
+domain = os.path.abspath(r’C:\Users\bululu\Documents\Untitled Folder\wine\benchmarks’)  
+info = os.path.join(domain,info)  
+df = pd.read_csv(info) 
+#规格化
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler(feature_range=(0, 1))
+df[['diff.score','alcohol']] = scaler.fit_transform(df[['diff.score','alcohol']])
+df[['diff.score','alcohol']].head()
+X1 = df['diff.score'].values.reshape(-1,1)
+X2 = df['alcohol'].values.reshape(-1,1)
+X = np.concatenate((X1,X2),axis=1)
+random_state = np.random.RandomState(1024)
+outliers_fraction = 0.25
+# Define seven outlier detection tools to be compared
+classifiers = {
+        'Angle-based Outlier Detector (ABOD)': ABOD(contamination=outliers_fraction),
+        'Cluster-based Local Outlier Factor (CBLOF)':CBLOF(contamination=outliers_fraction,check_estimator=False, random_state=random_state),
+        'Feature Bagging':FeatureBagging(LOF(n_neighbors=35),contamination=outliers_fraction,check_estimator=False,random_state=random_state),
+        'Histogram-base Outlier Detection (HBOS)': HBOS(contamination=outliers_fraction),
+        'Isolation Forest': IForest(contamination=outliers_fraction,random_state=random_state),
+        'K Nearest Neighbors (KNN)': KNN(contamination=outliers_fraction),
+        'Average KNN': KNN(method='mean',contamination=outliers_fraction)
+}
+xx , yy = np.meshgrid(np.linspace(0,1 , 200), np.linspace(0, 1, 200))
+ 
+for i, (clf_name, clf) in enumerate(classifiers.items()):
+    clf.fit(X)
+    # predict raw anomaly score
+    scores_pred = clf.decision_function(X) * -1
+        
+    # prediction of a datapoint category outlier or inlier
+    y_pred = clf.predict(X)
+    n_inliers = len(y_pred) - np.count_nonzero(y_pred)
+    n_outliers = np.count_nonzero(y_pred == 1)
+    plt.figure(figsize=(10, 10))
+    
+    # copy of dataframe
+    dfx = df
+    dfx['outlier'] = y_pred.tolist()
+    
+    # IX1 - inlier feature 1,  IX2 - inlier feature 2
+    IX1 =  np.array(dfx['diff.score'][dfx['outlier'] == 0]).reshape(-1,1)
+    IX2 =  np.array(dfx['alcohol'][dfx['outlier'] == 0]).reshape(-1,1)
+    
+    # OX1 - outlier feature 1, OX2 - outlier feature 2
+    OX1 =  dfx['diff.score'][dfx['outlier'] == 1].values.reshape(-1,1)
+    OX2 =  dfx['alcohol'][dfx['outlier'] == 1].values.reshape(-1,1)
+         
+    print('OUTLIERS : ',n_outliers,'INLIERS : ',n_inliers, clf_name)
+        
+    # threshold value to consider a datapoint inlier or outlier
+    threshold = stats.scoreatpercentile(scores_pred,100 * outliers_fraction)
+        
+    # decision function calculates the raw anomaly score for every point
+    Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()]) * -1
+    Z = Z.reshape(xx.shape)
+          
+    # fill blue map colormap from minimum anomaly score to threshold value
+    plt.contourf(xx, yy, Z, levels=np.linspace(Z.min(), threshold, 7),cmap=plt.cm.Blues_r)
+        
+    # draw red contour line where anomaly score is equal to thresold
+    a = plt.contour(xx, yy, Z, levels=[threshold],linewidths=2, colors='red')
+        
+    # fill orange contour lines where range of anomaly score is from threshold to maximum anomaly score
+    plt.contourf(xx, yy, Z, levels=[threshold, Z.max()],colors='orange')
+        
+    b = plt.scatter(IX1,IX2, c='white',s=20, edgecolor='k')
+    
+    c = plt.scatter(OX1,OX2, c='black',s=20, edgecolor='k')
+       
+    plt.axis('tight')  
+    
+    # loc=2 is used for the top left corner 
+    plt.legend(
+        [a.collections[0], b,c],
+        ['learned decision function', 'inliers','outliers'],
+        prop=matplotlib.font_manager.FontProperties(size=20),
+        loc=2)
+      
+    plt.xlim((0, 1))
+    plt.ylim((0, 1))
+    plt.title(clf_name)
+    plt.show()
+#auc
+X_train, X_test, y_train, y_test = \
+            train_test_split(X1, X2, test_size=0.4, random_state=random_state)
+ # standardizing data for processing
+X_train_norm, X_test_norm = standardizer(X_train, X_test)
+for clf_name, clf in classifiers.items():
+    t0 = time()
+    clf.fit(X_train_norm)
+    test_scores = clf.decision_function(X_test_norm)
+    t1 = time()
+    duration = round(t1 - t0, ndigits=4)
+
+    roc = round(roc_auc_score(y_test, test_scores), ndigits=4)
+    prn = round(precision_n_scores(y_test, test_scores), ndigits=4)
+
+    print('{clf_name} ROC:{roc}, precision @ rank n:{prn}, '
+          'execution time: {duration}s'.format(
+        clf_name=clf_name, roc=roc, prn=prn, duration=duration))
+
+    time_mat[i, classifiers_indices[clf_name]] = duration
+    roc_mat[i, classifiers_indices[clf_name]] = roc
+    prn_mat[i, classifiers_indices[clf_name]] = prn
+roc_list = roc_list + np.mean(roc_mat, axis=0).tolist()
+temp_df = pd.DataFrame(roc_list).transpose()
+temp_df.columns = df_columns
+roc_df = pd.concat([roc_df, temp_df], axis=0)
